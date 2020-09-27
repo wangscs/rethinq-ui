3c9d43891c8d97cc5eb61bacc57e80a0b984c3e4
Zachary Garcia <39964541+Zgarc@users.noreply.github.com>
2020-09-26 23:24:46 -0400

    Courses feature added

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Books from '../img/img2.jpg'
 
 const CourseCard = ({ courseContent }) => {
-	const { name, number } = courseContent;
+	const course = courseContent;
 	return (
 		<div>
-		
+
 			<Link to={`/course/`}>
-                <div className="course-card">
-					<div className="card-img-container"></div>
-					<h2 className="logo-font text-center">{name}</h2>
-					<h2 className="logo-font text-center">{number}</h2>
-                </div>
+				<div class="course-card">
+					<img class="card-img-top" src={Books} alt="Card image cap" />
+					<div class="course-card-body">
+						<h5 class="card-title">{course.name}</h5>
+						<p class="card-text">{course.code}</p>
+						<a href="#" class="btn btn-primary text-white">Book Tutor</a>
+					</div>
+				</div>
 			</Link>
 		</div>
 	);
