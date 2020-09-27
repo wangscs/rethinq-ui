5722aed5d8491644d91596910bdf077c7bf15781
Zachary Garcia <39964541+Zgarc@users.noreply.github.com>
2020-09-27 09:30:34 -0400

    Fixed add course button

--- src/components/CoursesOffered.js
+++ src/components/CoursesOffered.js
@@ -15,7 +15,7 @@ function listCourse(course, i) {
 
 function CoursesOffered(props) {
     const { myCourses } = props;
-    const history = useHistory();    
+    const history = useHistory();
 
     const handleClick = (e) => {
         e.preventDefault();
@@ -26,14 +26,14 @@ function CoursesOffered(props) {
         <div className="bottom-profile-components">
             <h3>Courses Offered</h3>
             <div class="card-body d-flex flex-column">
+                <button onClick={handleClick} class="btn btn-outline-primary mt-auto">Add Course</button>
 
-                {myCourses && myCourses.map( courseContent => (
+                {myCourses && myCourses.map(courseContent => (
                     <CourseOffDetails
                         key={courseContent.id}
                         courseID={courseContent.id}
                     />
                 ))}
-                <button onClick={handleClick} class="btn btn-primary text-white mt-auto">Add Course</button>
             </div>
         </div>
     );
