8b2264481406f5e68ed1f03d0359e618886b494c
Zachary Garcia <39964541+Zgarc@users.noreply.github.com>
2020-09-27 03:32:46 -0400

    Added teach course function

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import "./css/homepage.css";
 import "./css/explore.css";
 import "./css/coursecard.css";
+import "./css/profile.css";
 import { ContextProvider } from "./context/UserContext";
 
 ReactDOM.render(
